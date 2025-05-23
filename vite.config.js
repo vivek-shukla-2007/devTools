b3347e8c9cf0e23c28e0898198002d7d042ad9fc
Vivek Shukla <vivek.shukla.2007@gmail.com>
2025-05-23 13:51:24 +0100

    issue

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  root: path.resolve(__dirname, './'),
-  publicDir: path.resolve(__dirname, './public'),
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true,
-    strictPort: true,
-    open: '/'
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+  server: { open: true } 
 })
