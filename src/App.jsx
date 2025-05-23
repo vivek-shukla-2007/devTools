3448b5ea06d235162d77794ad9c411d81b90e40b
Vivek Shukla <vivek.shukla.2007@gmail.com>
2025-05-23 11:37:48 +0100

    rectifying mistakes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import HomePage from './features/home/HomePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './features/home/HomePage'; // <-- Only one import
 import JsonFormatter from './features/json-formatter/JsonFormatter';
 import CronGenerator from './features/cron-generator/CronGenerator';
 import Base64Tool from './features/base64/Base64Tool';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './features/home/HomePage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/json-formatter" element={<JsonFormatter />} />
-      <Route path="/cron-generator" element={<CronGenerator />} />
-      <Route path="/base64" element={<Base64Tool />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/json-formatter" element={<JsonFormatter />} />
+        <Route path="/cron-generator" element={<CronGenerator />} />
+        <Route path="/base64" element={<Base64Tool />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// Router dependencies
-import { BrowserRouter } from 'react-router-dom';
-
-// Create root and render app
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App /> {/* No BrowserRouter here */}
   </React.StrictMode>
 );
